21a1d5d756612957cd6e2b211e0de00af2025f44
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-04 00:45:15 +0000

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -54,15 +54,25 @@ ActiveRecord::Schema.define(version: 20200601183043) do
     t.integer  "category_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "sales", force: :cascade do |t|
     t.string   "name"
+    t.integer  "percent_off"
+    t.date     "starts_on"
+    t.date     "ends_on"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
-  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
